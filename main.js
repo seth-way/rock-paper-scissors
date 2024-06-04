095488fb24820063e1071c9bdb94fb6a74fec70a
seth-way <seth.r.way@gmail.com>
2024-06-04 12:45:19 -0400

    initial project setup

---
+++ main.js
@@ -0,0 +1,10 @@
+// <> <> GLOBAL VARIABLES <> <> //
+var options1;
+var options2;
+var game;
+var players;
+// <> <> DOM VARIABLES <> <> //
+
+// <> <> EVENT LISTENERS <> <> //
+
+// <> <> FUNCTIONS <> <> //
